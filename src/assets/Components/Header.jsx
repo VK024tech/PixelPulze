d5e0e9e6643ec7a50c8d71a3ade17684cbf3f236
Vivek Kandari <Kandarivivek260@gmail.com>
2025-07-09 21:07:57 +0530

    responsive-header

--- src/assets/Components/Header.jsx
+++ src/assets/Components/Header.jsx
@@ -1,20 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { motion } from "motion/react";
 import {
   ArrowLongRightIcon,
   MoonIcon,
   SunIcon,
+  Bars3Icon,
 } from "@heroicons/react/24/outline";
 
 export default function Header() {
   const [theme, setTheme] = useState(false);
+  const navOption = ["Home", "Support", "Pricing"];
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-  const navOption = ["Home", "Contact", "Pricing"];
+  /////////////////////resize width//////////////////////////
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
+  /////////////////////////////////////////////////////////////
 
   function navShow() {
     return navOption.map((curr) => {
       return (
-        <a key={curr} className="group relative py-2 px-4 cursor-pointer  rounded-md">
+        <a
+          key={curr}
+          className="group relative py-2 px-4 cursor-pointer  rounded-md"
+        >
           <span className="relative z-10">{curr}</span>
           <span className="absolute inset-0 scale-0 opacity-20 bg-buttonBg rounded-lg transition-all duration-300 ease-out group-hover:opacity-100 group-hover:scale-100 origin-center"></span>
         </a>
@@ -22,38 +40,56 @@ export default function Header() {
     });
   }
 
+  function navbar() {
+    if (windowWidth < 800) {
+      return <Bars3Icon className="size-6 my-4.5 " />;
+    } else {
+      return (
+        <>
+          <div className="flex gap-4">{navShow()}</div>
+          <div className="mx-4 flex gap-4 items-center min-w-57">
+            <div
+              onClick={() => {
+                setTheme(!theme);
+              }}
+              className="group relative py-2 px-4 cursor-pointer  rounded-md"
+            >
+              {theme && <MoonIcon className="size-6 relative z-10" />}
+              {!theme && <SunIcon className="size-6 relative z-10" />}
+              <span className="absolute inset-0 scale-0 opacity-20 bg-buttonBg rounded-lg transition-all duration-300 ease-out group-hover:opacity-100 group-hover:scale-100 origin-center"></span>
+            </div>
+
+            <motion.a
+              whileHover={{
+                translateX: 10,
+              }}
+              transition={{
+                duration: "0.5",
+                ease: "backInOut",
+              }}
+              className=" group px-4 icon-enter flex gap-3  w-full py-2.5 my-2 bg-gray text-white hover:text-brand ring-1 hover:ring-2 ring-brand hover:shadow-md/50 shadow-brand transition-all duration-300  rounded-2xl cursor-pointer"
+            >
+              Get Started
+              <ArrowLongRightIcon className="size-6 " />
+            </motion.a>
+          </div>
+        </>
+      );
+    }
+  }
+
   return (
-    <header className="md:fixed md:right-[50%] md:top-4 md:translate-x-[50%] md:rounded-md shadow-xs/90 shadow-gray-500  items-center  flex gap-12 justify-between  px-8 dark:bg-gray dark:text-white bg-white text-gray">
+    <header
+      className={`${
+        windowWidth < 800
+          ? ""
+          : "fixed right-[50%] top-4 translate-x-[50%] rounded-md"
+      }  shadow-xs/90 shadow-gray-500  items-center  flex gap-12 justify-between  px-8 dark:bg-gray dark:text-white bg-white text-gray`}
+    >
       <div className="p-1 mx-4">
         <a>PixelPulze</a>
       </div>
-      <div className="flex gap-4">{navShow()}</div>
-      <div className="mx-4 flex gap-4 items-center min-w-57">
-        <div
-          onClick={() => {
-            setTheme(!theme);
-          }}
-          className="group relative py-2 px-4 cursor-pointer  rounded-md"
-        >
-          {theme && <MoonIcon className="size-6 relative z-10" />}
-          {!theme && <SunIcon className="size-6 relative z-10" />}
-          <span className="absolute inset-0 scale-0 opacity-20 bg-buttonBg rounded-lg transition-all duration-300 ease-out group-hover:opacity-100 group-hover:scale-100 origin-center"></span>
-        </div>
-
-        <motion.a
-          whileHover={{
-            translateX: 10,
-          }}
-          transition={{
-            duration: "0.5",
-            ease: "backInOut",
-          }}
-          className=" group px-4 icon-enter flex gap-3  w-full py-2.5 my-2 bg-gray text-white hover:text-brand ring-1 hover:ring-2 ring-brand hover:shadow-md/50 shadow-brand transition-all duration-300  rounded-2xl cursor-pointer"
-        >
-          Get Started
-          <ArrowLongRightIcon className="size-6 " />
-        </motion.a>
-      </div>
+      {navbar()}
     </header>
   );
 }
