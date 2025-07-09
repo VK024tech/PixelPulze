22b587aa2ce7078f7398c6f65d9242522115e399
Vivek Kandari <Kandarivivek260@gmail.com>
2025-07-09 19:20:42 +0530

    Initialise

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./assets/Components/Header";
+
+function App() {
+  return (
+    <>
+      <div className="bg-black w-screen h-screen">
+        <Header />
+      </div>
+    </>
+  );
+}
+
+export default App;
